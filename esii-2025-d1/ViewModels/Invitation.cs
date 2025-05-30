c0559ed889f3210358568478d86f1e23024f95bc
.hensei <tiagoavrodrigues@gmail.com>
2025-05-30 11:51:14 +0100

    Added invitation service and ViewModel

---
+++ esii-2025-d1/ViewModels/Invitation.cs
@@ -0,0 +1,10 @@
+﻿using esii_2025_d1.Models;
+
+namespace esii_2025_d1.ViewModels
+{    public class Invitation
+    {
+        public UserInfo userInfo;
+        public ProjectUser invite;
+        public Project project;
+    }
+}
